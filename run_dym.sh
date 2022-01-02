fdcf2388ad27bde81b230a0b25305cfe5a143855
qym7 <qym2016@sjtu.edu.cn>
2022-01-02 18:02:48 +0800

    update self

---
+++ run_dym.sh
@@ -0,0 +1,8 @@
+model_path='./models/UM-val'
+output_path='./outputs/UM-val'
+log_path='./logs/UM-val'
+seed=0
+data_path='/DATA7_DB7/data/cju/20/BaSNet/dataset/THUMOS14'
+test_dataset='val'
+
+CUDA_VISIBLE_DEVICES=0 python -W ignore ./main.py --model_path ${model_path} --output_path ${output_path} --log_path ${log_path} --seed ${seed} --data_path ${data_path} --test_dataset ${test_dataset}
